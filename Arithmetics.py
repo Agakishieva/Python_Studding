e269d360bea4f512183e6abe6242d527c21cb055
Agakishieva <agakidhieva@gmail.com>
2025-11-26 17:07:28 +0300

    Arithmetics

---
+++ Arithmetics.py
@@ -0,0 +1,16 @@
+print(-11 % 5)
+
+print((31**331) % 20)
+
+n = int(input())
+
+print((((n + n) * n) - n) // n)
+
+number = int(input())
+a = number // 100
+print(a)
+b = (number // 10) % 10
+print(b)
+c = number % 10
+print(c)
+print(a + b + c)
